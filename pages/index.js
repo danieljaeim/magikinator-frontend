269e5cad414704dce5b22a71bdc556c2d960cd9f
Daniel Im <imdanieljae@gmail.com>
2023-11-29 18:32:50 -0800

    Added initial tailwind mockup for prototype

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,113 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image'
+import 'tailwindcss/tailwind.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const SERVER_URL = "http://localhost:5000/guess"
+    const SCRYFALL_IMG_URL = "https://api.scryfall.com/cards/named?format=image&fuzzy="
+    const [questions, setQuestions] = useState([]);
+    const [answers, setAnswers] = useState([]);
+    const [entropy, setEntropy] = useState(null);
+    const [bestQuestion, setBestQuestion] = useState("")
+    const [guessedCard, setGuessedCard] = useState("Fury Sliver")
+    const [guessedCardImg, setGuessedCardImg] = useState("/garfield.png")
+    const [loading, setLoading] = useState(true)
+    
+    useEffect(() => {
+        const getInitialData = async () => {
+            await fetch(SERVER_URL, {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                    'Access-Control-Allow-Origin': "*",
+                    "Access-Control-Allow-Headers": "Content-Type"
+                }
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    let bestQ = data['bestQuestion']
+                    let cardGuessedByServer = data['guessedCard']
+                    setBestQuestion(bestQ)
+                    setGuessedCard(cardGuessedByServer)
+                    setQuestions([...questions, bestQ])
+                    setLoading(false)
+                })
+        };
+        getInitialData()
+    }, [])
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    const answerQuestion = async (answer) => {
+        console.log("CALLED HANDLECLICK for " + answer)
+        await fetch(SERVER_URL, {
+            method: 'POST',
+            body: JSON.stringify({
+                "Questions": questions,
+                "Answers": [...answers, answer],
+                "Entropy": entropy
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                'Access-Control-Allow-Origin': "*",
+                "Access-Control-Allow-Headers": "Content-Type"
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                let bestQ = data['bestQuestion']
+                let cardGuessedByServer = data['guessedCard']
+                console.log(cardGuessedByServer)
+                let entropyVector = JSON.parse(data['entropyValue'])
+                setBestQuestion(bestQ)
+                setGuessedCard(cardGuessedByServer)
+                setEntropy(entropyVector)
+                setQuestions([...questions, bestQ])
+                setAnswers([...answers, answer])
+            })
+            .then(_ => {
+                fetch(SCRYFALL_IMG_URL + guessedCard, {
+                    method: 'GET'
+                })
+                    .then((response) => {
+                        if (response.status == 200) {
+                            setGuessedCardImg(response.url)
+                        }
+                    })
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+    }
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    return (
+        <div>
+            <div class="relative h-8 w-32">
+                <div class="absolute inset-x-0 top-0 h-16"> Magikinator </div>
+            </div> 
+            <div class="container w-full h-96 mx-auto max-w-full px-4 border-solid border">
+                <div class="flex flex-col justify-center">
+                    <img 
+                        src={guessedCardImg}
+                        alt="No Image Found for Card"
+                        class="self-center border-solid border max-w-lg rounded-lg" 
+                        width={150}
+                        height={200}
+                    />
+                    <div class="justify-end basis-1/2 h-96 border-solid border-2 border-black-500">
+                        {loading ? 
+                            <div class="text-center border-solid"> Loading best question </div> :
+                            <div class="">
+                                <div class="text-center text-sm border-solid"> {bestQuestion} </div>
+                                <div class="flex justify-center">
+                                    <button class="m-3 text-sm p-2 border-solid border border-black rounded" type="button" onClick={async () => answerQuestion("YES")}> Yes </button>
+                                    <button class="m-3 text-sm p-2 border-solid border border-black rounded" type="button" onClick={async () => answerQuestion("NO")} > No </button>
+                                    <button class="m-3 text-sm p-2 border-solid border border-black rounded" type="button" onClick={async () => answerQuestion("MAYBE")}> Maybe </button>
+                                    {/* <div> YOUR CURRENT GUESSED CARD IS: {guessedCard} </div> */}
+                                </div>
+                            </div>
+                        }       
+                    </div>
+                </div>
+            </div>
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  );
+    )
 }
